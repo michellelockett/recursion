dcabf92bad2cd329540599f7fcc7e9da19293abf
Michelle Lockett <shelldog42@gmail.com>
2017-09-30 20:29:46 -0700

    refactor to extract array and object helper functions

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,41 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 
+//https://stackoverflow.com/questions/22333101/recursive-json-stringify-implementation
 
 var stringifyJSON = function(obj) {
-
-  if (obj === null || obj === undefined) {
-  	return 'null';
-  } else if (obj === true) {
-  	return 'true';
-  } else if (obj === false) {
-  	return 'false';
-  } else if (typeof obj === 'number') {
-  	return obj.toString();
-  } else if (typeof obj === 'string') {
-  	return '\"' + obj + '"'
-  } else if (Array.isArray(obj)) {
-  	if (obj.length === 0) {
+	var type = typeof obj;
+	console.log(type);
+	//helper function to stringify arrays
+	var stringifyArray = function() {
+		if (obj.length === 0) {
   		return '[]';
   	} else if (obj.length === 1) {
   		return '[' + stringifyJSON(obj[0]) + ']';
   	} else {
-  		var result = '['
+  		var result = '[';
   		obj.forEach(function(element) {
   			if (obj.indexOf(element) === obj.length - 1) {
   				result += stringifyJSON(element);
   			} else {
   				result += stringifyJSON(element) + "," ;
   			}
-
-  		})
-  		result += ']'
+  		});
+  		result += ']';
   		return result;
   	}
-  } else if (typeof obj === 'function') {
-  	return;
-  } else if (typeof obj === 'object') {
-  	var result = '{';
+	}
+	//helper function to stringify objects
+	var stringifyObect = function() {
+		var result = '{';
   	if (Object.keys(obj).length === 0) {
   		return '{}';
   	} else if (Object.keys(obj).length === 1) {
@@ -45,21 +37,53 @@ var stringifyJSON = function(obj) {
   			result += '\"' + key.toString() + "\":";
   			result += stringifyJSON(obj[key]);
   		}
-  	} else {
- 		
-  		for (var key in obj) {
-  			
+  	} else {		
+  		for (var key in obj) {			
 	  		if (typeof obj[key] !== 'function' && obj[key] !== undefined) {	
 	  			result += '\"' + key.toString() + "\":";
 	  			result += stringifyJSON(obj[key]);
 	  			if (key !== (Object.keys(obj)[Object.keys(obj).length - 1])) {
-	  				result += ","
+	  				result += ",";
 	  			}
-  			}
-	  		
+  			}	  		
   		}
   	}
   	result += '}';
   	return result;
-  }
-};
+	}
+
+	switch (type) {
+		case 'boolean':
+			if(obj === true) {
+				return 'true';
+			} else {
+				return 'false';
+			}
+			break;
+		case 'undefined':
+			return;
+			break;
+		case 'number':
+			return obj.toString();
+			break;
+		case 'string':
+			return '\"' + obj + '"';
+			break;
+		case 'function':
+			return;
+			break;
+		case 'object':
+			if(obj === null) {
+				return 'null';
+			} else if (Array.isArray(obj)) {
+				return stringifyArray();
+			} else {
+				return stringifyObect();
+			}
+			break;
+		default:
+			return;
+	}
+
+}
+
